b3c194a463efcff65b3c90e22bbbe29404f3cb2e
Shubhendu Vaid <vaidshubhendu@gmail.com>
2025-05-19 12:30:15 +0100

    added blog details

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -5,28 +5,37 @@ import { ArrowRight } from "lucide-react";
 
 const blogPosts = [
   {
-    title: "Leveraging AI for Enhanced Data Analytics",
+    title: "Leveraging Adapter Design Pattern for Seamless CMS Integrations",
     excerpt:
-      "How artificial intelligence is transforming the way organizations extract insights from their data.",
-    date: "April 18, 2025",
+      "In the ever-evolving landscape of content management systems (CMS), creating robust integrations with various platforms can be a challenging yet crucial task. I am excited to share our successful journey in implementing the Adapter Design Pattern to streamline and enhance our CMS integrations.",
+    date: "Feb 20, 2024",
+    readTime: "2 min read",
+    category: "Architecture",
+    link: "https://medium.com/@vaidshubhendu/leveraging-adapter-design-pattern-for-seamless-cms-integrations-15b8544f4bed",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1280/format:webp/1*uMLUCtCQRW6gADkOEGgDzA.png",
+  },
+  {
+    title: "From Engineer to Engineering Manager",
+    excerpt:
+      "As we navigate the journey from an engineer to engineering manager, it's essential to recognise that there are distinct styles or approaches that engineers often fall into when making this transition, and by understanding these one can decide on where they need to improve on.",
+    date: "April 21, 2024",
     readTime: "5 min read",
-    category: "Data Science",
+    category: "Engineering Management",
+    link: "https://www.linkedin.com/pulse/from-engineer-engineering-manager-shubhendu-vaid-cvk9e/?trackingId=1YUcV2vHey4HItOIn47WQw%3D%3D",
+    image:
+      "https://media.licdn.com/dms/image/v2/D4E12AQGSmSJDDWIbMA/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1713726280779?e=1753315200&v=beta&t=K3oBfkTVOm_TafJiRfHA7Nm8Yh2lzgQZWgT2b-R8rZ8",
   },
   {
-    title: "Building Data Products That Users Love",
+    title: "Efficiently retrieving data from 1 billion records.",
     excerpt:
-      "Key principles for designing data products with excellent user experience and meaningful insights.",
-    date: "April 2, 2025",
-    readTime: "8 min read",
-    category: "Product Management",
-  },
-  {
-    title: "The Future of Business Intelligence",
-    excerpt:
-      "Emerging trends in BI tools and how they're reshaping decision-making processes in modern businesses.",
-    date: "March 15, 2025",
-    readTime: "6 min read",
-    category: "Analytics",
+      "Retrieving large-scale datasets efficiently is key to maintaining competitive, high-performing applications. For many developers and engineers, the challenge isn’t just in storing massive amounts of data but in ensuring that data retrieval remains quick and reliable, even as datasets grow to millions or billions of records.",
+    date: "Nov 10, 2024",
+    readTime: "10 min read",
+    category: "Backend Engineering",
+    link: "https://www.linkedin.com/pulse/efficiently-retrieving-data-from-1-billion-records-shubhendu-vaid-bo7we/?trackingId=tsR%2FejSlrwXo1B8Q1B4dYw%3D%3D",
+    image:
+      "https://media.licdn.com/dms/image/v2/D4E12AQHKTfr0nBfbiQ/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1731010828278?e=1753315200&v=beta&t=FarMT9LpgYCQyxEZOJc1YrKuD-Q0Dip6DYFoqrgKeZc",
   },
 ];
 
@@ -58,8 +67,13 @@ const BlogSection = () => {
               <Card key={index} className="card-hover animate-fade-in">
                 <CardContent className="p-0">
                   <div className="h-48 bg-gradient-to-r from-navy-700 to-teal-700 relative">
+                    <img
+                      src={post.image}
+                      alt={post.title}
+                      className="w-full h-full object-cover rounded-tl-lg rounded-tr-lg"
+                    />
                     <div className="absolute bottom-4 left-4">
-                      <span className="px-2 py-1 bg-white text-navy-800 text-xs font-medium rounded">
+                      <span className="px-2 py-1 border-1 border-black shadow-2xl bg-white text-navy-800 text-xs  font-medium rounded">
                         {post.category}
                       </span>
                     </div>
@@ -74,12 +88,19 @@ const BlogSection = () => {
                   </div>
                 </CardContent>
                 <CardFooter className="px-6 pb-6 pt-0">
-                  <Button
-                    variant="link"
-                    className="p-0 h-auto flex items-center gap-1 text-primary font-medium"
+                  <a
+                    href={post?.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block"
                   >
-                    Read more <ArrowRight size={16} />
-                  </Button>
+                    <Button
+                      variant="link"
+                      className="p-0 h-auto flex items-center gap-1 text-primary font-medium"
+                    >
+                      Read more <ArrowRight size={16} />
+                    </Button>
+                  </a>
                 </CardFooter>
               </Card>
             ))}
